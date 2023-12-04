4450cf52da4660ed0228e9afc5c356b1198df6e6
Julian Köpke <julian.koepke@gmail.com>
2023-12-05 00:39:53 +0100

    golf 04

--- src/a04.erl
+++ src/a04.erl
@@ -1,24 +1,8 @@
 -module(a04).
 -export([a/1, b/1]).
 
-a(In) ->
-    P = [points(parse(L)) || L <- In],
-    lists:sum(P).
-
-parse(L) ->
-    [_Card, Numbers] = re:split(L, "\\s*\\:\\s*", [trim]),
-    [Left, Right] = re:split(Numbers, "\\s*\\|\\s*", [trim]),
-    Set = fun(S) -> sets:from_list(re:split(S, "\\s+", [trim])) end,
-    sets:size(sets:intersection(Set(Left), Set(Right))).
-
-points(0) -> 0;
-points(C) -> round(math:pow(2, C - 1)).
-
-b(In) -> collect(0, [{1, parse(L)} || L <- In]).
-
-collect(Acc, []) -> Acc;
-collect(Acc, [{C, 0} | T]) -> collect(Acc + C, T);
-collect(Acc, [{C, W} | T]) -> collect(Acc + C, push(T, W, C)).
-
-push(L, 0, _C) -> L;
-push([{H, G} | T], N, C) -> [{H + C, G} | push(T, N - 1, C)].
+a(I)->P=[x(p(L))||L<-I],lists:sum(P). p(L)->[_,N]=re:split(L,":\\s*"),[A,B]=re:
+split(N,"\\s*\\|\\s*"),S=fun(S)->sets:from_list(re:split(S,"\\s+"))end,sets:size
+(sets:intersection(S(A),S(B))). x(0)->0;x(C)->round(math:pow(2,C-1)). b(I)->c(0,
+[{1,p(L)}||L<-I]). c(A,[])->A;c(A,[{C,0}|T])->c(A+C,T);c(A,[{C,W}|T])->c(A+C,d(T
+,W,C)). d(L,0,_)->L;d([{H,G}|T],N,C)->[{H+C,G}|d(T,N-1,C)].
