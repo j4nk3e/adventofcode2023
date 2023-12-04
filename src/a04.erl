fe1bbc00c3adccc46edbb7b9e1337d4df1b50ab8
Julian Köpke <julian.koepke@gmail.com>
2023-12-05 00:26:12 +0100

    solution 04 with list comprehensions

--- src/a04.erl
+++ src/a04.erl
@@ -2,22 +2,19 @@
 -export([a/1, b/1]).
 
 a(In) ->
-    G = lists:map(fun parse/1, In),
-    P = lists:map(fun points/1, G),
+    P = [points(parse(L)) || L <- In],
     lists:sum(P).
 
 parse(L) ->
-    [_Card, Numbers] = re:split(L, "\\s*\\:\\s*", [{return, list}, trim]),
-    [Left, Right] = re:split(Numbers, "\\s*\\|\\s*", [{return, list}, trim]),
-    Set = fun(S) -> sets:from_list(re:split(S, "\\s+", [{return, list}, trim])) end,
+    [_Card, Numbers] = re:split(L, "\\s*\\:\\s*", [trim]),
+    [Left, Right] = re:split(Numbers, "\\s*\\|\\s*", [trim]),
+    Set = fun(S) -> sets:from_list(re:split(S, "\\s+", [trim])) end,
     sets:size(sets:intersection(Set(Left), Set(Right))).
 
 points(0) -> 0;
 points(C) -> round(math:pow(2, C - 1)).
 
-b(In) ->
-    G = lists:map(fun parse/1, In),
-    collect(0, lists:zip(lists:duplicate(length(G), 1), G)).
+b(In) -> collect(0, [{1, parse(L)} || L <- In]).
 
 collect(Acc, []) -> Acc;
 collect(Acc, [{C, 0} | T]) -> collect(Acc + C, T);
